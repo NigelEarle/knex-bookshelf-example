308adeea9ff73c06151c12e9d5e299369dbaa3d1
nwre87 <nwre87@gmail.com>
2017-11-17 12:30:31 -1000

    add migration scripts

---
+++ migrations/20171117121723_create_tasks_table.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('tasks', function(table){
+    table.increments();
+    table.string('title').notNullable();
+    table.string('description').notNullable();
+    table.boolean('is_complete').defaultTo(false);
+    table.integer('user_id').references('id').inTable('users');
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('tasks');
+};
